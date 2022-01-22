d946508978b42449b640a501faf9355e1aeacc2a
morswin22 <xmorswinx@gmail.com>
2022-01-22 13:38:08 +0100

    :sparkles: Add random and grid distribution of trees

--- DataGenerator/Assets/Scenes/Forestation.cs
+++ DataGenerator/Assets/Scenes/Forestation.cs
@@ -3,11 +3,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+class Tree
+{
+    public string name;
+    public GameObject prefab;
+    public Vector3 size;
+
+    public Tree(string path, string name)
+    {
+        this.name = name;
+        // Load the model
+        this.prefab = new OBJLoader().Load($"{path}/{name}.obj", $"{path}/{name}.mtl");
+        this.prefab.name = $"Tree_{name}";
+        this.prefab.transform.Rotate(-90f, 0f, 0f, Space.Self);
+        // Get max size from prefab's children
+        Bounds bounds = new Bounds();
+        foreach (Transform child in prefab.transform)
+        {
+            bounds.Encapsulate(child.gameObject.GetComponent<Renderer>().bounds);
+        }
+        size = bounds.size;
+    }
+}
+
 public class Forestation : MonoBehaviour
 {
     GameObject sp;
     GameObject ground;
-    List<GameObject> trees = new List<GameObject>();
+    List<Tree> trees = new List<Tree>();
     Vector3 maximumCoordinates;
     Vector3 minimumCoordinates;
     public float minimumDistanceFromTheEdge = 5;
@@ -40,28 +63,87 @@ public class Forestation : MonoBehaviour
     void LoadSingleTree()
     {
         // TODO: think how to select .obj files 
-        var path = Application.dataPath + "/Resources/ForestObjects/EU43_5";
-        var tree = new OBJLoader().Load($"{path}.obj", $"{path}.mtl");
-        tree.name = $"tree_{trees.Count + 1}";
+        var path = Application.dataPath + "/Resources/ForestObjects";
+        var tree = new Tree(path, "EU43_5");
+        tree.prefab.name = $"Tree_{trees.Count + 1}";
 
-        tree.transform.position = new Vector3(
+        tree.prefab.transform.position = new Vector3(
             Random.Range(minimumCoordinates.x + minimumDistanceFromTheEdge, maximumCoordinates.x - minimumDistanceFromTheEdge),
             Random.Range(minimumCoordinates.y, maximumCoordinates.y) + 0.5f,
             Random.Range(minimumCoordinates.z + minimumDistanceFromTheEdge, maximumCoordinates.z - minimumDistanceFromTheEdge));
-
-        tree.transform.Rotate(-90f, 0f, 0f, Space.Self);
-        trees.Add(tree);
 
         // .tif extension is a problem - to import properly with textures,
         // I've converted every .tif file to .png and I've edited .mtl file
         // (replaced every occurrence of '.tif' with '.png')
     }
 
+    void LoadTrees(string path, List<string> names)
+    {
+        // Load all trees from the names list
+        foreach (var name in names)
+        {
+            var tree = new Tree(path, name);
+            tree.prefab.transform.position = new Vector3(0.0f, -tree.size.y, 0.0f);
+            trees.Add(tree);
+        }
+    }
+
+    void PlaceTreesRandom(int count)
+    {
+        // Place trees randomly
+        for (int i = 0; i < count; i++)
+        {
+            var tree = Instantiate(trees[Random.Range(0, trees.Count)].prefab);
+            tree.transform.position = new Vector3(
+                Random.Range(minimumCoordinates.x + minimumDistanceFromTheEdge, maximumCoordinates.x - minimumDistanceFromTheEdge),
+                Random.Range(minimumCoordinates.y, maximumCoordinates.y) + 0.5f,
+                Random.Range(minimumCoordinates.z + minimumDistanceFromTheEdge, maximumCoordinates.z - minimumDistanceFromTheEdge));
+        }
+    }
+
+    void PlaceTreesGrid(float gap, float maxRandomOffset)
+    {
+        // Constrain parameters
+        if (gap < 0.0f)
+            gap = 0.0f;
+        if (maxRandomOffset < 0.0f)
+            maxRandomOffset = 0.0f;
+        // Find max size of trees from Tree.size
+        Vector3 maxSize = new Vector3();
+        foreach (var tree in trees)
+        {
+            if (tree.size.x > maxSize.x)
+                maxSize.x = tree.size.x;
+            if (tree.size.z > maxSize.z)
+                maxSize.z = tree.size.z;
+        }
+        // Calculate step and offset
+        float xStep = maxSize.x + gap;
+        float zStep = maxSize.z + gap;
+        float xOffset = (((maximumCoordinates.x - minimumCoordinates.x - 2 * minimumDistanceFromTheEdge) / xStep) % 1) * xStep * 0.5f;
+        float zOffset = (((maximumCoordinates.z - minimumCoordinates.z - 2 * minimumDistanceFromTheEdge) / zStep) % 1) * zStep * 0.5f;
+        // Place trees in a grid
+        for (float x = minimumCoordinates.x + minimumDistanceFromTheEdge + xOffset; x <= maximumCoordinates.x - minimumDistanceFromTheEdge; x += xStep)
+        {
+            for (float z = minimumCoordinates.z + minimumDistanceFromTheEdge + zOffset; z <= maximumCoordinates.z - minimumDistanceFromTheEdge; z += zStep)
+            {
+                var tree = Instantiate(trees[Random.Range(0, trees.Count)].prefab);
+                tree.transform.position = new Vector3(
+                    x + Random.Range(-maxRandomOffset, maxRandomOffset),
+                    Random.Range(minimumCoordinates.y, maximumCoordinates.y) + 0.5f, 
+                    z + Random.Range(-maxRandomOffset, maxRandomOffset));
+            }
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         GetEdgesOfTheWorld();
-        LoadSingleTree();
+        // LoadSingleTree();
+        LoadTrees(Application.dataPath + "/Resources/ForestObjects", new List<string> { "EU43_5" });
+        // PlaceTreesRandom(10);
+        PlaceTreesGrid(5.0f, 3.0f);
     }
 
     // Update is called once per frame
